172dd296a186b82762a1a6158910895eceddc532
jahidhasan8 <jahidhasan1932@gmail.com>
2022-10-31 14:46:25 +0600

    project setup, home section footer section done

---
+++ src/Pages/Home/About/About.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import person from '../../../assets/images/about_us/person.jpg'
+import parts from '../../../assets/images/about_us/parts.jpg'
+const About = () => {
+    return (
+        <div className="hero min-h-screen bg-base-200">
+            <div className="hero-content flex-col lg:flex-row">
+                <div className='w-1/2 relative'>
+                    <img src={person} alt="" className=" rounded-lg shadow-2xl w-4/5 h-full" />
+                    <img src={parts} alt="" className=" rounded-lg shadow-2xl  border-8 absolute right-5 top-1/2 w-3/5 " />
+                </div>
+                <div className='w-1/2'>
+                    <p className="text-2xl text-orange-600 font-bold">
+                        About Us
+                    </p>
+                    <h1 className="text-5xl font-bold">We are qualified <br /> & of experience <br /> in this field</h1>
+                    <p className="py-6">There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. </p>
+                    <p className="py-6">
+                        the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.
+                    </p>
+                    <button className="btn btn-primary bg-orange-600 ">Get More Info</button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default About;
